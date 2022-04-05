e4a71f1fc15ac4c9bb73d238e2b872084e6780f3
Nirmit <nirmitji22@gmail.com>
2022-04-06 01:36:40 +0530

    initial commit

---
+++ src/components/ContactDetail.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import user from '../images/contact-image.jpg'
+
+const ContactDetail = (props) => {
+
+    const location = useLocation()
+    const { name, email } = location.state.contact
+
+    return (
+        <div className="main" style={{marginTop: "5rem"}}>
+            <div className="ui card centered">
+                <div className="image">
+                    <img src={user} alt="user" />
+                </div>
+                <div className="content">
+                    <div className="header">{name}</div>
+                    <div className="description">{email}</div>
+                </div>
+            </div>
+            <div className="ui container center aligned">
+                <Link to="/">
+                    <button className="ui button blue">
+                        Go Back
+                    </button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default ContactDetail
